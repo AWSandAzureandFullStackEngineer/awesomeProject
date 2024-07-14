f292a8e67789224a3b9e9182c512558f6d0889ab
AWSandAzureandFullStackEngineer <151389135+AWSandAzureandFullStackEngineer@users.noreply.github.com>
2024-07-14 00:30:30 -0500

    first commit

---
+++ awesomeProject/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := "Hello, World!"
+	body, err := ioutil.ReadAll(rr.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(body) != expected {
+		t.Errorf("handler returned unexpected body: got %q want %q",
+			body, expected)
+	}
+}
