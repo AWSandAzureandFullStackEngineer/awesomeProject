29d475e5231c98df1bcce01521c59e7027d73538
Steven McDonald <stevenmcdonald@Stevens-MacBook-Pro.local>
2024-07-14 20:12:50 -0500

    update api.

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/hello", handler)
+	http.HandleFunc("/api/hello", handler)
 
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
